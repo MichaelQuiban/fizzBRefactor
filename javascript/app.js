b6335cfe98779587524e9b56f8deaf22055b6ebe
Michael Q <MichaelQuiban@outlook.com>
2016-07-16 14:18:52 -0700

    Added jquery, text field checks, CSS.
    Design on Button was added.
    Checks to the text area has been added.

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,20 @@
-function fizzbuzzFun(a, b){
-	
-	return welcomeFizz;
-}
+$(document).ready(function() {
+	$('#submit').on('click', function(e) {
+		e.preventDefault();
+		var number = +$('#query').val();
+ 
+		// make sure section is empty before clearing
+		if (!$('section').is(':empty')) $('section').html('');
 
-welcomeFizz();
-fizzbuzzFun();
+		// make sure span is empty before clearing message
+		if (!$('span').is(':empty')) $('span').html('');
+
+		// check if number is integer
+		if (number % 1 === 0) {
+			appendFizzBuzz(number);
+		} else {
+			$('span').append('Input must be an integer.');
+		}
+
+	});
+});
