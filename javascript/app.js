6ce724b4f2f110953358ea327dca0b54caf4e2d8
Michael Q <MichaelQuiban@outlook.com>
2016-07-17 10:55:50 -0700

    Javascript Changes, JQuery Changes, HTML changes.
    Made our section into an unordered list.
    Added input validation.
    Now it's time to append our results to the HTML <ul></ul> once that form
    is submitted.

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,34 @@
-$(document).ready(function() {
-	$('#submit').on('click', function(e) {
+$(document).ready(function(){
+	$('#submit').on('click',function(e){
 		e.preventDefault();
-		var number = +$('#query').val();
- 
-		// make sure section is empty before clearing
-		if (!$('section').is(':empty')) $('section').html('');
+		var fizzNumber = +$('#fizzNumber').val();
 
-		// make sure span is empty before clearing message
+		// Validate an empty unordered list.
+		if (!$('ul').is(':empty')) $('ul').html('');
+
+		// make sure span is empty.
 		if (!$('span').is(':empty')) $('span').html('');
 
-		// check if number is integer
-		if (number % 1 === 0) {
-			appendFizzBuzz(number);
-		} else {
-			$('span').append('Input must be an integer.');
+		// check if the number is an integer
+		if (fizzNumber % 1 === 0){
+			fizzBuzz(fizzNumber);
+		}else {
+			$('span').append('Input must be an integer');
 		}
-
 	});
 });
+
+function fizzBuzz(){
+	for(i = 0; i <= 100; i++){
+		if(i % 3===0 && i % 5===0){
+			console.log("Fizzbuzz!");
+		}else if(i % 3===0){
+			console.log("Fizz!");
+		}else if(i % 5===0){
+			console.log("Buzz!");
+		}else{
+			console.log(i);
+		}
+	}
+}
+fizzBuzz();
